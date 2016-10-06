0b8768134ef80e4dfd6bba06086459623b63dde6
waikei1917 <waikei1917oct@gmail.com>
2016-10-05 21:08:32 -0600

    SQL

--- PythonSQLTest/code/main_test.py
+++ PythonSQLTest/code/main_test.py
@@ -1,1 +1,17 @@
-print("Hello World");
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="192.168.1.74",
+    user="oldblackwave",
+    passwd="28Oklmfts",
+    db="mysql"
+)
+
+cur = db.cursor()
+
+cur.execute("SELECT * from user")
+
+for row in cur.fetchall():
+    print(row[0])
+
+db.close
